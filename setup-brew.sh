40a19fa4ad723d9a896362c59e09df22538a6a04
Alexander Nyberg <alexandernyberg@ip-80-90-1-244.ax5z.com>
2023-04-23 22:29:32 +0200

    Added brew setup file

---
+++ setup-brew.sh
@@ -0,0 +1,27 @@
+brew upgrade
+brew tap homebrew/cask-versions
+
+# CLI Tools
+brew install git
+brew install node
+brew install 
+
+# Formulae
+brew install pyenv
+brew install unixodbc
+brew install openssl@1.1
+brew install openssl@3
+brew install php
+
+# Cask
+brew install --cask google-chrome
+brew install --cask google-chrome-canary
+brew install --cask firefox
+brew install --cask linearmouse
+brew install --cask stats
+brew install --cask iterm2
+brew install --cask mongodb-compass
+brew install --cask fork
+brew install --cask mullvadvpn
+
+brew cleanup
