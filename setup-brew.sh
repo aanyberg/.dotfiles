b4caf5fb9b4589c7b74001ba755c78cd84a17723
Alexander Nyberg <alexandernyberg@ip-80-90-1-244.ax5z.com>
2023-04-24 10:55:01 +0200

    Added setup file for macOS

--- setup-brew.sh
+++ setup-brew.sh
@@ -4,7 +4,6 @@ brew tap homebrew/cask-versions
 # CLI Tools
 brew install git
 brew install node
-brew install 
 
 # Formulae
 brew install pyenv
@@ -18,10 +17,26 @@ brew install --cask google-chrome
 brew install --cask google-chrome-canary
 brew install --cask firefox
 brew install --cask linearmouse
-brew install --cask stats
 brew install --cask iterm2
-brew install --cask mongodb-compass
 brew install --cask fork
 brew install --cask mullvadvpn
+brew install --cask cryptomator
+brew install --cask displaylink
+brew install --cask docker
+brew install --cask knockknock
+brew install --cask logi-options-plus
+brew install --cask transmission
+brew install --cask tunnelblick
+brew install --cask spotify
+brew install --cask stats
+brew install --cask bettertouchtool
+brew install --cask alfred
+brew install --cask microsoft-teams
+brew install --cask onedrive
+brew install --cask microsoft-auto-update
+brew install --cask microsoft-office
+brew install --cask bitwarden
+brew install --cask appcleaner
 
+# Cleanup after install
 brew cleanup
---
+++ setup-macos.sh
@@ -0,0 +1,2 @@
+# Adjust Dock auto hide time duration
+defaults write com.apple.dock autohide-time-modifier -float 0.4
