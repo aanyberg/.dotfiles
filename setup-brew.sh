8505c5e7d5be9146cf2d656af975d1a8398c725f
Alexander Nyberg <alexandernyberg@ip-80-90-1-244.ax5z.com>
2023-05-01 18:50:02 +0200

    Cleanup

--- setup-brew.sh
+++ setup-brew.sh
@@ -23,7 +23,6 @@ brew install --cask mullvadvpn
 brew install --cask cryptomator
 brew install --cask displaylink
 brew install --cask docker
-brew install --cask knockknock
 brew install --cask logi-options-plus
 brew install --cask transmission
 brew install --cask tunnelblick
@@ -35,7 +34,6 @@ brew install --cask microsoft-teams
 brew install --cask onedrive
 brew install --cask microsoft-auto-update
 brew install --cask microsoft-office
-brew install --cask microsoft-edge
 brew install --cask bitwarden
 brew install --cask appcleaner
 brew install --cask plex-media-server
